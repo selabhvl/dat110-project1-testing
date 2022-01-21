a812c2870974ca59a91f30d57f4224a97406efb0
Lars Kristensen <lmkr@hib.no>
2022-01-21 22:00:54 +0100

    added test for RPC encap/decap

--- dat110-project1-tests/src/no/hvl/dat110/rpc/tests/TestRPCUtils.java
+++ dat110-project1-tests/src/no/hvl/dat110/rpc/tests/TestRPCUtils.java
@@ -9,6 +9,34 @@ import no.hvl.dat110.rpc.RPCUtils;
 class TestRPCUtils {
 
 	@Test
+	void testDecapsulate() {
+		
+		byte[] rpcrequest = {0,1,2};
+		
+		byte[] payload = RPCUtils.decapsulate(rpcrequest);
+		
+		assertEquals(2,payload.length);
+		assertEquals(1,payload[0]);
+		assertEquals(2,payload[1]);
+		
+	}
+	
+	@Test
+	void testEncapsulate() {
+		
+		byte[] payload = {1,2};
+		
+		byte[] rpcrequest = RPCUtils.encapsulate((byte)0, payload);
+		
+		assertEquals(0,rpcrequest[0]);
+		assertEquals(1,rpcrequest[1]);
+		assertEquals(2,rpcrequest[2]);
+		
+	}
+	
+
+	
+	@Test
 	void testMarshallString() {
 		
 		String str = "teststring";
